b46357ffd111bae1daa91ba246234ba6536fc5e4
Sujit Gupta <sujitgupta9850@gmail.com>
2024-09-30 17:16:29 +0530

    worked on the project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10 " href="#login">
+          Something
+        </Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+}
