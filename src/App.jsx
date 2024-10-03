5e1cbc43415102559ac5219922c2d39887662358
Sujit Gupta <sujitgupta9850@gmail.com>
2024-10-03 20:05:22 +0530

    worked on pricing,roadmap,footer components and edited other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 export default function App() {
   return (
@@ -15,7 +17,9 @@ export default function App() {
         <Benefits />
         <Collaboration />
         <Services />
-        <Pricing/>
+        <Pricing />
+        <Roadmap />
+        <Footer/>
       </div>
       <ButtonGradient />
     </>
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,5 +1,51 @@
+import Section from "./Section";
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <Section crosses className="overflow-hidden" id="pricing">
+      <div className="container relative z-2 ">
+        <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
+          <img
+            src={smallSphere}
+            alt="sphere"
+            className="relative z-1"
+            width={255}
+            height={255}
+          />
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+            <img
+              src={stars}
+              alt="stars"
+              className="w-full "
+              width={950}
+              height={400}
+            />
+          </div>
+        </div>
+        <Heading
+          className="text-center font-bold"
+          tag="Get started with Brainwave"
+          title="Use Once, Pay forever"
+        />
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+        <div className="flex justify-center mt-10">
+          <a
+            href="/"
+            className="text-xs font-code font-bold tracking-wider uppercase border-b py-2"
+          >
+            See the full details
+          </a>
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default Pricing;
